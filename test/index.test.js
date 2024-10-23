ba597223f87f36d997fec623c18f9911670480a9
DianaLlamoca <89529542+DianaLlamoca@users.noreply.github.com>
2024-10-23 11:06:26 -0500

    Update index.test.js

--- test/index.test.js
+++ test/index.test.js
@@ -1,9 +1,24 @@
-//Obtengo la prueba que se encuentra en el directorio /src/index
-const prueba = require('../src/index');
+//Uso supertest, pues me permitirá probar la API
+const request=require('supertest');
 
-test('resultado',async ()=>{
-    //Acá espero el resultado, al ser una promesa uso la sintaxis await dentro de una función asíncrona
-    const resultado=await prueba();
-    //Acá realizo la comprobación de que el resultado devuelto por la promesa sea "Prueba correcta" y la prueba sea validada.
-    expect(resultado).toBe("Prueba correcta");
-});
+//Obtengo la instancia del app express en el directorio /src/index
+const app=require('../src/index');
+
+const RespuestaEsperada = [
+    { id: 1, name: 'user1', email: 'user1@test.com' },
+    { id: 2, name: 'user2', email: 'user2@test.com' }
+  ];
+  
+  //Aquí es donde realizo las pruebas
+  describe('GET /users', () => {
+    it('DEBERÍA RETORNAR UNA LISTA DE USUARIOS', async () => {
+      const response = await request(app).get('/users');
+
+      //Verifico que haya sido la conexión exitosa (200)
+      expect(response.statusCode).toBe(200);   
+
+      //Valido que los datos de la ruta al realizar la petición get a /users sea igual
+      expect(response.body).toEqual(RespuestaEsperada);   
+  
+    },10000); //Establezco un tiempo de espera de 10 segundos
+  });
