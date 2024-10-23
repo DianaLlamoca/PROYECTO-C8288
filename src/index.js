521014f5f321b02bff189fb5c358772a753951c4
DianaLlamoca <89529542+DianaLlamoca@users.noreply.github.com>
2024-10-23 09:40:05 -0500

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,14 +1,10 @@
-//Acá defino una aplicación express básica
-const servidor_express=require("express");
-const aplicacion_basica=servidor_express();
-const puerto=3000;
-
-//Defino la ruta y que solo me devuelva un "Hola mundo"
-aplicacion_basica.get("/",(req,res)=>{
-    res.send("Despliegue correcto");
-})
-
-//Para que la app esté en escucha en el puerto definido
-aplicacion_basica.listen(puerto,()=>{
-    console.log(`Aplicación corriendo en http://localhost:${puerto}`);
-});
+//Acá defino una Prueba sencilla, que devolverá una promesa que se resuelve en 2 segundos
+function Prueba(){
+    return new Promise((resolve,reject)=>{
+        setTimeout(()=>{
+            resolve("Prueba correcta");
+        },2000);
+    });
+}
+//Exporto la función para usarla en el archivo .test.js
+module.exports=Prueba;
